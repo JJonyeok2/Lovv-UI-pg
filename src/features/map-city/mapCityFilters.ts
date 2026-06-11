dfd1dc9828ff800a97753b7e0d6c4f25b26145ac
쪼녁2 <jacob122405@gmail.com>
2026-06-11 16:13:10 +0900

    Revert "feat(ui): restore MVP frontend experience"
    This reverts commit 2f802740410786705405e304d4a1b57d5413b7eb.

--- src/features/map-city/mapCityFilters.ts
+++
@@ -1,8 +0,0 @@
-import type { SmallCityTheme } from './smallCities'
-
-export const mapFilterThemes: SmallCityTheme[] = ['온천', '바다', '미식', '전통', '자연', '예술']
-
-const mapFilterThemeSet = new Set<SmallCityTheme>(mapFilterThemes)
-
-export const getVisibleMapThemes = (themes: SmallCityTheme[]) =>
-  themes.filter((theme) => mapFilterThemeSet.has(theme))
